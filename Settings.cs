230b6d5b87bafa7f27db3b260423acf7c1313a0d
matheusrmartinez <matheus.rmartinez@gmail.com>
2020-03-05 11:53:15 -0300

    Correction in entityframeworkcore version

--- Settings.cs
+++ Settings.cs
@@ -4,7 +4,6 @@ namespace Shop
 {
     public static class Settings
     {
-        public static Guid NewGuid { get { return Guid.NewGuid();} }
         public static string Secret = "f567fd9999sssd765300slapq0";
     }
 
